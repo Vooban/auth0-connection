0263b90ac02ac86581588f5cc037c457e7a859fa
Jose Wallet <jose.ouellet@vooban.com>
2018-08-01 14:18:59 -0400

    added put and delete

--- auth0connection/Program.cs
+++ auth0connection/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Configuration;
 using System.IO;
+using System.Linq;
 using System.Net;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
@@ -10,13 +11,36 @@ namespace auth0connection
 {
     internal class Program
     {
+        private static readonly HttpStatusCode[] OkStatus = { HttpStatusCode.OK, HttpStatusCode.Accepted, HttpStatusCode.Created, HttpStatusCode.NoContent };
+
         private static void Main(string[] args)
         {
+            var apiGetUrl = ConfigurationManager.AppSettings["api_get_url"];
+            var apiPostUrl = ConfigurationManager.AppSettings["api_post_url"];
+            var apiPutUrl = ConfigurationManager.AppSettings["api_put_url"];
+            var apiDeleteUrl = ConfigurationManager.AppSettings["api_delete_url"];
+
             var authorizationToken = Authorization();
 
-            ApiGet(authorizationToken);
-
-            ApiPost(authorizationToken);
+            if (authorizationToken != null)
+            {
+                if (!string.IsNullOrEmpty(apiGetUrl))
+                {
+                    ApiGet(authorizationToken, apiGetUrl);
+                }
+                if (!string.IsNullOrEmpty(apiPostUrl))
+                {
+                    ApiPost(authorizationToken, apiPostUrl);
+                }
+                if (!string.IsNullOrEmpty(apiPutUrl))
+                {
+                    ApiPut(authorizationToken, apiPutUrl);
+                }
+                if (!string.IsNullOrEmpty(apiDeleteUrl))
+                {
+                    ApiDelete(authorizationToken, apiDeleteUrl);
+                }
+            }
 
             // Leave console open
             Console.WriteLine("// Press any key to quit...");
@@ -27,9 +51,15 @@ namespace auth0connection
         {
             // Initialize a request to Auth0
             var authUrl = ConfigurationManager.AppSettings["auth_token_url"];
+            if (string.IsNullOrEmpty(authUrl))
+            {
+                Console.WriteLine("// No auth0 url set");
+                return null;
+            }
+
             var client = new RestClient(authUrl);
             var request = new RestRequest(Method.POST);
-            request.AddHeader("content-type", "application/json");
+            request.AddHeader("Content-Type", "application/json");
 
             // Filling up the request body using configs in app.config
             var content = new RequestBodyContent();
@@ -46,10 +76,9 @@ namespace auth0connection
             return responseContent;
         }
 
-        private static void ApiGet(ResponseBodyContent authorizationToken)
+        private static void ApiGet(ResponseBodyContent authorizationToken, string apiUrl)
         {
             // Initialize a GET request to our API
-            var apiUrl = ConfigurationManager.AppSettings["api_get_url"];
             var client = new RestClient(apiUrl);
             var request = new RestRequest(Method.GET);
 
@@ -59,11 +88,11 @@ namespace auth0connection
 
             // GET an object from the API
             var response = client.Execute(request);
-            var apiResponseSucceeded = response.StatusCode == HttpStatusCode.OK;
+            var apiResponseSucceeded = OkStatus.Contains(response.StatusCode);
 
             // Output the api response
             Console.WriteLine($"// API GET Status: {(apiResponseSucceeded ? "Succeeded" : $"Failed ({response.StatusCode})")}");
-            if (apiResponseSucceeded)
+            if (!string.IsNullOrEmpty(response.Content))
             {
                 Console.WriteLine($"// API GET Result:\n{response.Content}");
             }
@@ -72,13 +101,12 @@ namespace auth0connection
             // e.g. var myClass = JsonConvert.DeserializeObject<ResponseBodyContent>(response.content);
         }
 
-        private static void ApiPost(ResponseBodyContent authorizationToken)
+        private static void ApiPost(ResponseBodyContent authorizationToken, string apiUrl)
         {
             // Initialize a POST request to our API
-            var apiUrl = ConfigurationManager.AppSettings["api_post_url"];
             var client = new RestClient(apiUrl);
             var request = new RestRequest(Method.POST);
-            request.AddHeader("content-type", "application/json");
+            request.AddHeader("Content-Type", "application/json");
 
             // Set the token in the authorization header
             // TODO: Don't request a new token every time, save it in your cache.
@@ -89,17 +117,69 @@ namespace auth0connection
             // e.g. var json = JsonConvert.SerializeObject(myClass);
             var apiPostJsonPath = ConfigurationManager.AppSettings["api_post_json_path"];
             var apiPostJsonBody = !File.Exists(apiPostJsonPath) ? "" : new StreamReader(apiPostJsonPath).ReadToEnd();
-            request.AddJsonBody(apiPostJsonBody);
+            request.AddParameter("Application/Json", apiPostJsonBody, ParameterType.RequestBody);
 
             // POST an object to the API
             var response = client.Execute(request);
-            var apiResponseSucceeded = response.StatusCode == HttpStatusCode.OK;
+            var apiResponseSucceeded = OkStatus.Contains(response.StatusCode);
 
             // Output the api response
             Console.WriteLine($"// API POST Status: {(apiResponseSucceeded ? "Succeeded" : $"Failed ({response.StatusCode})")}");
-            if (apiResponseSucceeded)
+            if (!string.IsNullOrEmpty(response.Content))
             {
                 Console.WriteLine($"// API POST Result:\n{response.Content}");
+            }
+        }
+
+        private static void ApiPut(ResponseBodyContent authorizationToken, string apiUrl)
+        {
+            // Initialize a PUT request to our API
+            var client = new RestClient(apiUrl);
+            var request = new RestRequest(Method.PUT);
+            request.AddHeader("Content-Type", "application/json");
+
+            // Set the token in the authorization header
+            // TODO: Don't request a new token every time, save it in your cache.
+            request.AddHeader("Authorization", authorizationToken.ToString());
+
+            // Set the request JSON body from a path to test the api
+            // TODO: Instead, use a class to serialize it to a JSON object and PUT this to the API.
+            // e.g. var json = JsonConvert.SerializeObject(myClass);
+            var apiPutJsonPath = ConfigurationManager.AppSettings["api_put_json_path"];
+            var apiPutJsonBody = !File.Exists(apiPutJsonPath) ? "" : new StreamReader(apiPutJsonPath).ReadToEnd();
+            request.AddParameter("Application/Json", apiPutJsonBody, ParameterType.RequestBody);
+
+            // PUT an object to the API
+            var response = client.Execute(request);
+            var apiResponseSucceeded = OkStatus.Contains(response.StatusCode);
+
+            // Output the api response
+            Console.WriteLine($"// API PUT Status: {(apiResponseSucceeded ? "Succeeded" : $"Failed ({response.StatusCode})")}");
+            if (!string.IsNullOrEmpty(response.Content))
+            {
+                Console.WriteLine($"// API PUT Result:\n{response.Content}");
+            }
+        }
+
+        private static void ApiDelete(ResponseBodyContent authorizationToken, string apiUrl)
+        {
+            // Initialize a DELETE request to our API
+            var client = new RestClient(apiUrl);
+            var request = new RestRequest(Method.DELETE);
+
+            // Set the token in the authorization header
+            // TODO: Don't request a new token every time, save it in your cache.
+            request.AddHeader("Authorization", authorizationToken.ToString());
+
+            // DELETE an object from the API
+            var response = client.Execute(request);
+            var apiResponseSucceeded = OkStatus.Contains(response.StatusCode);
+
+            // Output the api response
+            Console.WriteLine($"// API DELETE Status: {(apiResponseSucceeded ? "Succeeded" : $"Failed ({response.StatusCode})")}");
+            if (!string.IsNullOrEmpty(response.Content))
+            {
+                Console.WriteLine($"// API DELETE Result:\n{response.Content}");
             }
         }
     }
@@ -121,7 +201,7 @@ namespace auth0connection
         }
     }
 
-    // Auth Response
+    // Auth0 Response
     internal class ResponseBodyContent
     {
         public string AccessToken { get; set; }
